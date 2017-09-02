4bcc2afaed312a13a4eb357b0a24cf74badfaace
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-02 18:13:15 -0300

    Update

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 define('IS_API', false);
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/Mbh/Settings/config.php';
-require __DIR__ . '/Mbh/App.php';
 
-use \Mbh\App;
+use Mbh\App;
 
-App::registerAutoload();
+// App::registerAutoload();
 
 $app = new App;
 
