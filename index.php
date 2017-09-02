46d51f80ffae73ca66b88adc1c36a00d0a193b2e
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-02 19:13:38 -0300

    Update

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 
+/**
+ * Defines the directory in which the framework is installed
+ * @example "/" If to access the framework we place http://url.com in the URL, or http://localhost
+ * @example "/mbh-framework/" if to access the framework we place http://url.com/mbh-framework, or http://localhost/mbh-framework/
+*/
+define('__ROOT__', '/skeleton/');
+define('URL', 'http://localhost/skeleton/');
 define('IS_API', false);
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor/mbh-framework/rest/Mbh/Settings/config.php';
 
 use Mbh\App;
-
-// App::registerAutoload();
 
 $app = new App;
 
 $app->any('/', function() {
-    /* code */
+    return "MBHFramework is working!";
 });
 
 $app->firewall()
