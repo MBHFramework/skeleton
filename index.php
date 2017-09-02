572b97051aa2391e3475356366f71070c5f29d45
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-02 17:42:18 -0300

    My first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+define('IS_API', false);
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/Mbh/Settings/config.php';
+require __DIR__ . '/Mbh/App.php';
+
+use \Mbh\App;
+
+App::registerAutoload();
+
+$app = new App;
+
+$app->any('/', function() {
+    /* code */
+});
+
+$app->firewall()
+    ->debug()
+    ->run();
